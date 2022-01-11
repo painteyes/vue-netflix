224230866b0bd6a59c2a8917816c9f0b51e7fac6
painteyes <andreantoniocchipinti@gmail.com>
2022-01-11 15:26:21 +0100

    scaffolding

---
+++ src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+
+    <section>
+
+        <div class="logo">
+            <img src="https://logospng.org/download/netflix/logo-netflix-4096.png" alt="logo">       
+        </div>
+
+      <div class="searchbar">
+            <input v-model="searchedText" type="text">
+      </div>
+
+    </section>
+
+</template>
+
+// -------------------------------------------------------------------
+
+<script>
+
+export default {
+    name: 'Header',
+    data: function (){
+        return {
+            searchedText: '',
+        }
+    }
+}
+
+</script>
+
+// -------------------------------------------------------------------
+
+<style lang="scss" scoped>
+
+template{
+    background-color: black;
+}
+
+section{
+
+    background-color: black;
+    display: flex;
+    justify-content: space-between;
+    
+    img{
+        width: 100px;
+        margin: auto 20px;
+    }
+
+    .searchbar {
+        margin: auto 20px;
+    }
+
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <section>
+
+        <ul>
+            <li>
+
+            </li>
+        </ul>
+
+    </section>
+
+</template>
+
+// -------------------------------------------------------------------
+
+<script>
+
+export default {
+    name: 'Main',
+}
+
+</script>
+
+// -------------------------------------------------------------------
+
+<style lang="scss" scoped>
+
+section{
+    background-color: gray;
+    height: 100vh;
+}
+
+</style>
