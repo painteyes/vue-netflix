3ae6cc36642142752e06a411181ddcab7e874d6b
painteyes <andreantoniocchipinti@gmail.com>
2022-01-13 15:00:04 +0100

    bug-flickering-covers

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,78 +1,94 @@
 <template>
     <section>
 
-        <div class="cover">
+        <div class="cover"> 
 
-            <template v-if="isMovie">
-               <img :src="'https://image.tmdb.org/t/p/w342'+ movieObject.poster_path" alt="">
-            </template>
+            <!-- Front -->
+            <div class="cover-front">
 
-            <template v-if="isSeries">
-               <img :src="'https://image.tmdb.org/t/p/w342'+ seriesObject.poster_path" alt="">
-            </template>
+                <template v-if="isMovie">
+                    <img :src="'https://image.tmdb.org/t/p/w342'+ movieObject.poster_path" alt="">
+                </template>
 
+                <template v-if="isSeries">
+                <img :src="'https://image.tmdb.org/t/p/w342'+ seriesObject.poster_path" alt="">
+                </template>
+            </div>
+
+            <!-- Retro -->
+            <div class="cover-retro">
+
+                <ul>
+
+                    <!-- Title -->
+                    <li v-if="isMovie">
+                        <span>Titolo: </span> {{ movieObject.title }} 
+                    </li>
+
+                    <li v-if="isSeries">
+                        <span>Titolo: </span> {{ seriesObject.name }} 
+                    </li>
+                    
+                    <!-- Original title -->
+                    <li v-if="isMovie">
+                        <span>Titolo originale: </span> {{ movieObject.original_title }} 
+                    </li>
+
+                    <li v-if="isSeries">
+                        <span>Titolo originale: </span> {{ seriesObject.original_name }} 
+                    </li>
+                    
+                    <!-- Language -->
+                    <li v-if="isMovie">
+
+                        <template v-if="flagsList.includes(movieObject.original_language)">
+                            <span>Lingua: </span>
+                            <img class="flag" :src="require(`../assets/${movieObject.original_language}.svg`)" alt="" >
+                        </template> 
+
+                        <template v-else>
+                            <span>Lingua: </span> {{movieObject.original_language}}
+                        </template> 
+                    </li>
+
+                    <li v-if="isSeries">
+
+                        <template v-if="flagsList.includes(seriesObject.original_language)">
+                            <span>Lingua: </span>
+                            <img class="flag" :src="require(`../assets/${seriesObject.original_language}.svg`)" alt="" >
+                        </template> 
+
+                        <template v-else>
+                            <span>Lingua: </span> {{ seriesObject.original_language }}
+                        </template> 
+                    </li>
+
+                    <!-- Vote -->
+                    <li v-if="isMovie">
+                        
+                        <i class="fas fa-star" v-for="number,index in getNumberOfStars(movieObject.vote_average)" :key='index'></i>
+                        <i class="far fa-star" v-for="number,index in (5 - getNumberOfStars(movieObject.vote_average))" :key='index'></i>
+                        
+                    </li>
+
+                    <li v-if="isSeries">
+
+                        <i class="fas fa-star" v-for="number,index in getNumberOfStars(seriesObject.vote_average)" :key='index'></i>
+                        <i class="far fa-star" v-for="number,index in (5 - getNumberOfStars(seriesObject.vote_average))" :key='index'></i>
+
+                    </li>
+
+                    <!-- Overview -->
+                    <li v-if="isMovie">
+                        <span>Overview: </span>{{ movieObject.overview }} 
+                    </li>
+
+                    <li v-if="isSeries">
+                        <span>Overview: </span> {{ seriesObject.overview }} 
+                    </li>
+                </ul>  
+            </div>
         </div>
-
-        <ul>
-
-            <!-- title -->
-            <li v-if="isMovie">
-                Titolo: {{ movieObject.title }} 
-            </li>
-
-            <li v-if="isSeries">
-                Titolo: {{ seriesObject.name }} 
-            </li>
-            
-            <!-- original title -->
-            <li v-if="isMovie">
-                 Titolo: {{ movieObject.original_title }} 
-            </li>
-
-            <li v-if="isSeries">
-                 Titolo: {{ seriesObject.original_name }} 
-            </li>
-            
-            <!-- original language -->
-            <li v-if="isMovie">
-
-                <template v-if="flagsList.includes(movieObject.original_language)">
-                    <img class="flag" :src="require(`../assets/${movieObject.original_language}.svg`)" alt="" >
-                </template> 
-
-                <template v-else>
-                    Lingua: {{movieObject.original_language}}
-                </template> 
-            </li>
-
-            <li v-if="isSeries">
-
-                <template v-if="flagsList.includes(seriesObject.original_language)">
-                    <img :src="require(`../assets/${seriesObject.original_language}.svg`)" alt="" >
-                </template> 
-
-                <template v-else>
-                    Lingua: {{ seriesObject.original_language }}
-                </template> 
-            </li>
-
-            <!-- vote -->
-            <li v-if="isMovie">
-                 
-                <i class="fas fa-star" v-for="number,index in getNumberOfStars(movieObject.vote_average)" :key='index'></i>
-                <i class="far fa-star" v-for="number,index in (5 - getNumberOfStars(movieObject.vote_average))" :key='index'></i>
-                
-            </li>
-
-            <li v-if="isSeries">
-
-                <i class="fas fa-star" v-for="number,index in getNumberOfStars(seriesObject.vote_average)" :key='index'></i>
-                <i class="far fa-star" v-for="number,index in (5 - getNumberOfStars(seriesObject.vote_average))" :key='index'></i>
-
-            </li>
-
-        </ul>  
-
     </section>
 </template>
 
@@ -116,13 +132,15 @@ export default {
 
 section{
 
-    border: 2px solid red;
+    
     margin-right: 40px;
     margin-bottom: 40px;
+    background-color: black;
+    color: white;
 
     padding: 20px;
-    // height: 300px;
-    width: 200px ;
+    // height: 700px;
+    // width: 200px ;
     
     ul{  
         // padding: 20px;
@@ -132,7 +150,36 @@ section{
 
     .flag{
         width: 30px ;
+        vertical-align: middle;
+        padding-left: 5px ;
     }
+
+    .cover-retro{
+        width: 342px;
+        span{
+            font-weight: bold;
+            color: whitesmoke;
+        }
+    }
+
+    .cover-retro {
+        display: none;
+    }
+
+    .cover:hover {
+
+        cursor: pointer;
+
+        .cover-front {
+            display: none;
+        }
+
+        .cover-retro {
+            display: block;
+        }  
+    }
+
+     
 
 }
 
